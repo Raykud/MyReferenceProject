24292ca431049478f4f787e679fb7fc25a5d26b2
Raykud <jorge@isdigital.me>
2021-12-16 11:47:17 -0600

    Delete features/firebaseoprs directory

--- features/firebaseoprs/src/main/java/com/ref/firebaseoprs/interfaces/ClickListener.kt
+++
@@ -1,5 +0,0 @@
-package com.ref.firebaseoprs.interfaces
-
-interface ClickListener {
-  fun  onPdfClicked(imageURL : String)
-}
--- features/firebaseoprs/src/main/java/com/ref/firebaseoprs/models/TaxInfo.kt
+++
@@ -1,3 +0,0 @@
-package com.ref.firebaseoprs.models
-
-data class TaxInfo(val employeeEmail : String = "",val taxDetailType:String = "",val taxDetailAmount:String = "",val uploadUrl:String ="")
